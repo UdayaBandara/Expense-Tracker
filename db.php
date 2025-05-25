b29e7eeffe0639b247a0ee2c9056265ac02b5e2b
Udaya Bandara <neranjanaudaya@gmail.com>
2025-05-25 15:41:08 +0530

    add database connection

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'localhost';
+$db = 'expense_db';
+$user = 'root';
+$pwd = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pwd, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
